d288d2ad063b338c82ce35c157c7fdac52f8d024
Manohar Castelino <mcastelino@users.noreply.github.com>
2018-03-23 13:50:12 -0700

    Create struct_in_enum.rs
    How should I really do a struct in an enum. This does not seem right

---
+++ struct_in_enum.rs
@@ -0,0 +1,36 @@
+struct Pointer {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+enum Message {
+    Quit,
+    Value(i32),
+    Point(i32, i32, i32),
+    EPointer(Pointer),
+}
+
+fn process_message(msg: Message) {
+    match msg {
+        Message::Quit => println!("Quit\n"),
+        Message::Value(r) => println!("Value: {}\n", r),
+        Message::Point(x, ..) => println!("Point {}\n", x),
+        Message::EPointer(p) => println!("Pointer {}\n", p.x),
+    };
+}
+
+fn main() {
+    let msg1 = Message::Value(100);
+    let msg2 = Message::Point(77, 88, 99);
+    let msg3 = Message::Quit;
+    let msg4 = Message::EPointer(Pointer {
+        x: 33,
+        y: 44,
+        z: 55,
+    });
+
+    process_message(msg1);
+    process_message(msg2);
+    process_message(msg3);
+    process_message(msg4);
+}
