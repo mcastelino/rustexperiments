d966bc6e11e6151ee1ab8e1542f15bde09e42301
Manohar Castelino <mcastelino@users.noreply.github.com>
2018-03-30 14:19:45 -0700

    Create lifetimes_needed.rs

---
+++ lifetimes_needed.rs
@@ -0,0 +1,30 @@
+fn foo<'a>(x: &'a u32, _y: &'a u32) -> &'a u32 {
+    x
+}
+
+fn foox<'a, 'b>(x: &'a u32, _y: &'b u32) -> &'a u32 {
+    x
+}
+
+fn fooy<'a, 'b>(_x: &'a u32, y: &'b u32) -> &'b u32 {
+    y
+}
+
+fn main() {
+    let x = 12;
+    let y = 13;
+
+    foo(&x, &y);
+
+    let _z: &u32 = {
+        let y = 42;
+        foox(&x, &y)
+        //foo(&x, &y) //will fail
+        //fooy(&x, &y) //will fail
+    };
+
+    let _z: &u32 = {
+        let x = 42;
+        fooy(&x, &y)
+    };
+}
